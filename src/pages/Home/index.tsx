a3b93cf9b4c6355eb54a3e4af7e7de8fe1264ea0
Sergey Novikov <synopticum@gmail.com>
2020-11-25 16:45:50 +0300

    Add GSAP draggable

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ErrorBoundary from 'src/components/ErrorBoundary';
 import { asyncFetchRectangles } from 'src/providers/reducers/rectangles.slice';
 import styled from 'styled-components';
@@ -8,6 +8,8 @@ import { RootState } from 'src/providers/reducers';
 import List from 'src/components/List';
 import { useAppDispatch as useDispatch } from 'src/providers/store';
 import { unwrapResult } from '@reduxjs/toolkit';
+import gsap, { TweenLite } from 'gsap';
+import Draggable from 'gsap/Draggable';
 
 const Content = styled.div`
   width: 100%;
@@ -19,13 +21,36 @@ const Try = styled.div`
   text-decoration: underline;
 `;
 
+const Area = styled.div`
+  width: 800px;
+  height: 400px;
+  background-color: black;
+  position: relative;
+  margin-top: 10px;
+
+  .box {
+    background-color: #91e600;
+    text-align: center;
+    font-family: Asap, Avenir, Arial, sans-serif;
+    width: 196px;
+    height: 100px;
+    line-height: 100px;
+    color: black;
+    position: absolute;
+    border-radius: 10px;
+  }
+`;
+
 type Props = {
   increment: typeof increment;
 };
 
+gsap.registerPlugin(Draggable);
+
 const Home: React.FC<Props> = () => {
   const dispatch = useDispatch();
   const employees = useSelector((state: RootState) => state.employees);
+  const refContainer = useRef(null);
 
   useEffect(() => {
     dispatch(asyncFetchRectangles(123));
@@ -37,9 +62,22 @@ const Home: React.FC<Props> = () => {
       .then(res => console.log(res));
   };
 
+  useEffect(() => {
+    const draggables = Draggable.create('.box', {
+      bounds: refContainer.current,
+      type: 'x,y',
+    });
+    const area = draggables[0];
+
+    area.applyBounds(refContainer.current);
+  }, []);
+
   return (
     <ErrorBoundary>
       <Content>
+        <Area ref={refContainer}>
+          <div className="box" />
+        </Area>
         <List items={employees} />
       </Content>
     </ErrorBoundary>
